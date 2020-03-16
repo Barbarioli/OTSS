f091bf920821f53dc8f4c7c54d7fa08f07ef1ed8
Barbarioli <bbarbarioli@hotmail.com>
2020-03-16 14:46:34 -0500

    stationary/

---
+++ stationary/multivariate_stationary_subsampler.py
@@ -0,0 +1,51 @@
+import math
+import time
+import numpy as np
+from random import randint
+
+def ms_subsampler(epsilon, delta, number_variables, variable_ranges, block_size, sample):
+
+	epsilon = epsilon
+	delta = delta
+	t = 1
+	k = 0
+	upper_bound = np.empty(number_variables)
+	upper_bound.fill(10000000000000)
+	lower_bound = np.empty(number_variables)
+	lower_bound.fill(-1000000000000)
+	beta = 1.5
+	p = 1.1
+	c = delta * (p-1)/p
+	last = None
+	values = np.array([])
+
+	#Checking for stopping condition
+	while (((epsilon + 1) * lower_bound < (1 - epsilon) * upper_bound).all):
+
+		#initial_values = [randint(0,len(sample)-(block_size+1)) for i in range(number_variables)]
+		initial_values = randint(0,len(sample)-(block_size+1))
+
+		if (t % 10 == 0):
+			print('sample size: ', t)
+		#print(initial_values, initial_values+block_size)
+		np.append(values, sample[int(initial_values):int(initial_values+block_size)])
+		#print(values)
+		t += block_size
+
+		#Adding new value
+
+		if t > np.floor(beta ** k):
+			k += 1
+			alpha = np.floor(beta ** k) / np.floor(beta ** (k - 1))
+			dk = c / (0.00000000000001 + (math.log(k, p) ** p))
+			x = -alpha * np.log(dk)/3
+
+		
+		ct = np.std(values, axis = 1) * np.sqrt(np.absolute(2 * x / t)) + 3 * variable_ranges * x / t
+		lower_bound = np.maximum(lower_bound, np.abs(np.mean(values,axis = 1))-ct)
+		#print('lower bound: ', lower_bound)
+		upper_bound = np.minimum(upper_bound, np.abs(np.mean(values, axis = 1))+ct)
+		#print('upper bound: ', upper_bound)
+
+
+	return values
--- stationary/univariate_stationary_subsampler.py
+++ stationary/univariate_stationary_subsampler.py
@@ -1,152 +1,48 @@
-#import cv2
-#from dlcv.utils import *
-#from dlcv.struct import Operator, Box
 import math
 import time
 import numpy as np
 from random import randint
-"""
-class EBGS(Operator):
-	EBGStop (Empirical Bernstein Stopping) as in the paper:
-	Volodymyr Mnih, Csaba Szepesvári, and Jean-Yves Audibert. 2008. Empirical Bernstein stopping. 
-	In Proceedings of the 25th international conference on Machine learning (ICML '08). 
-	ACM, New York, NY, USA, 672-679. DOI=http://dx.doi.org/10.1145/1390156.1390241
-	
-	def __init__(self, epsilon, delta, variable_range):
-		EBGS is an adaptative sampling algorithm that uses a geometric sampling schedule 
-		with a near-optimal stopping rule for relative error estimation on bounded random variable
 
-		Args:
-			epsilon (float): 
-			delta (float): user definided confidence, i.e., the event happens with 
-			probability at least (1-delta)
-			variable_range (float): range of the variable being measured
-		
-		self.epsilon = epsilon
-		self.delta = delta
-		self.range = variable_range
-		self.t = 1
-		self.k = 0
-		self.upper_bound = 10000000000000
-		self.lower_bound = -1000000000000
-		self.beta = 1.5
-		self.p = 1.1
-		self.filter="object"
-		self.c = self.delta * (self.p-1)/self.p
-		self.indicator = []
-		self.region = Box(200,550,350,750)
-		self.last = None
+def us_subsampler(epsilon, delta, variable_range, block_size, sample):
+
+	epsilon = epsilon
+	delta = delta
+	t = 1
+	k = 0
+	upper_bound = 10000000000000
+	lower_bound = -1000000000000
+	beta = 1.5
+	p = 1.1
+	c = delta * (p-1)/p
+	last = None
+	values = []
 
 
-	def __iter__(self):
-		self.frame_iter = iter(self.video_stream)
-		self.super_iter()
-		return self
+	#Checking for stopping condition
+	while ((epsilon + 1) * lower_bound < (1 - epsilon) * upper_bound):
+
+		initial_value = randint(0,len(sample)-(block_size+1))
+
+		if (t % 10 == 0):
+			print('sample size: ', t)
+		values.append(sample[initial_value:initial_value+block_size])
+		t += block_size
+
+		#Adding new value
+
+		if t > np.floor(beta ** k):
+			k += 1
+			alpha = np.floor(beta ** k) / np.floor(beta ** (k - 1))
+			dk = c / (0.00000000000001 + (math.log(k, p) ** p))
+			x = -alpha * np.log(dk)/3
+
+		ct = np.std(values) * np.sqrt(2 * x / t) + 3 * variable_range * x / t
+		lower_bound = max(lower_bound, np.abs(np.mean(values))-ct)
+		#print('lower bound: ', lower_bound)
+		upper_bound = min(upper_bound, np.abs(np.mean(values))+ct)
+		#print('upper bound: ', upper_bound)
 
 
-	def __next__(self):
-		out = next(self.frame_iter)
+	return values
 
-
-		#Checking for stopping condition
-		if (self.epsilon + 1) * self.lower_bound < (1 - self.epsilon) * self.upper_bound:
-			self.t += 1
-			if self.last == None:
-				pass
-			elif 'bounding_boxes' in self.last:
-			#print('number of bounding boxes:',len(self.last['bounding_boxes']))
-				cnt = 0
-				for label, pt in self.last['bounding_boxes']:
-					box = Box(*pt)
-					if label == self.filter and self.region.contains(box):
-						cnt += 1
-					else:
-						pass
-				if cnt == 0:
-					self.indicator.append(0)
-					
-				else:
-					#print('1')
-					self.indicator.append(1)
-			
-			#Adding new value
-
-			if self.t > np.floor(self.beta ** self.k):
-				self.k += 1
-				self.alpha = np.floor(self.beta ** self.k) / np.floor(self.beta ** (self.k - 1))
-				self.dk = self.c / (0.00000000000001 + (math.log(self.k, self.p) ** self.p))
-				self.x = -self.alpha * np.log(self.dk) / 3
-
-			values = np.asarray(self.indicator)
-			ct = np.std(values)* np.sqrt(2 * self.x / self.t) + 3 * self.range * self.x / self.t
-			self.lower_bound = max(self.lower_bound, np.abs(np.mean(values))-ct)
-			self.upper_bound = min(self.upper_bound, np.abs(np.mean(values))+ct)
-		
-
-			self.last = out
-			return out
-		else:
-			return self.__next__()
-
-
-	def _serialize(self):
-		return {'epsilon': self.epsilon, 'delta': self.delta, 'range': self.range}
-"""
-class subsampler(object):
-
-	def __init__(self, epsilon, delta, variable_range, block_size, block_type, sample):
-	"""	
-	Args:
-			epsilon (float): 
-			delta (float): user definided confidence, i.e., the event happens with 
-			probability at least (1-delta)
-			variable_range (float): range of the variable being measured
-			sample (float): sample to be evaluated
-	"""	
-		self.epsilon = epsilon
-		self.delta = delta
-		self.range = variable_range
-		self.t = 1
-		self.k = 0
-		self.upper_bound = 10000000000000
-		self.lower_bound = -1000000000000
-		self.beta = 1.5
-		self.p = 1.1
-		self.c = self.delta * (self.p-1)/self.p
-		self.last = None
-		self.block_size = block_size
-		self.block_type = block_type
-
-	def __iter__(self):
-		self.iter = iter(self.sample)
-		self.super_iter()
-		return self
-
-
-	def __next__(self):
-		out = next(self.iter)
-
-				#Checking for stopping condition
-		if (self.epsilon + 1) * self.lower_bound < (1 - self.epsilon) * self.upper_bound:
-			self.t += 1
-			if self.last == None:
-				#?
-			#Adding new value
-
-			if self.t > np.floor(self.beta ** self.k):
-				self.k += 1
-				self.alpha = np.floor(self.beta ** self.k) / np.floor(self.beta ** (self.k - 1))
-				self.dk = self.c / (0.00000000000001 + (math.log(self.k, self.p) ** self.p))
-				self.x = -self.alpha * np.log(self.dk) / 3
-
-			values = np.asarray(self.indicator)
-			ct = np.std(values)* np.sqrt(2 * self.x / self.t) + 3 * self.range * self.x / self.t
-			self.lower_bound = max(self.lower_bound, np.abs(np.mean(values))-ct)
-			self.upper_bound = min(self.upper_bound, np.abs(np.mean(values))+ct)
-		
-
-			self.last = out
-			return out
-		else:
-			return self.__next__()
 
